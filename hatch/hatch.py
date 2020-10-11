250ff17f7b498cb284177caa0b28d43abb15257b
Bernie Pope <bjpope@unimelb.edu.au>
2020-10-11 22:59:56 +1100

    Command line option to set aesthetic style

--- hatch/hatch.py
+++ hatch/hatch.py
@@ -41,6 +41,7 @@ DEFAULT_PLOT_WIDTH = 8
 DEFAULT_PLOT_HEIGHT = 8 
 DEFAULT_PLOT_NAME = "plot"
 DEFAULT_ORIENTATION = "v"
+DEFAULT_STYLE = "darkgrid"
 
 try:
     PROGRAM_VERSION = pkg_resources.require(PROGRAM_NAME)[0].version
@@ -118,6 +119,9 @@ def parse_args():
     common_arguments.add_argument(
         '--sample', metavar='NUM', required=False, type=int,
         help='Sample NUM rows from the input data')
+    common_arguments.add_argument(
+        '--style', choices=['darkgrid', 'whitegrid', 'dark', 'white', 'ticks'], required=False, default=DEFAULT_STYLE,
+        help=f'Aesthetic style of plot. Allowed values: %(choices)s. Default: %(default)s.')
     common_arguments.add_argument(
         '--navalues', metavar='STR', required=False, type=str,
         help='Treat values in this space separated list as NA values. Example: --navalues ". - !"')
@@ -693,6 +697,7 @@ def save(options, df):
 
 def main():
     options = parse_args()
+    sns.set_style(options.style)
     init_logging(options.logfile)
     make_output_directories(options)
     df = read_data(options)
