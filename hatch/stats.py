3782892329376a9319610c16ccbb76baecede927
Bernie Pope <bjpope@unimelb.edu.au>
2021-10-24 21:09:20 +1100

    Make isnorm return a dataframe

--- hatch/stats.py
+++ hatch/stats.py
@@ -20,7 +20,7 @@ import hatch.constants as const
 
 class IsNorm(CommandBase, name="isnorm"):
     description = "Test whether numerical features differ from a normal distribution."
-    category = "summary information"
+    category = "transformation"
 
     def __init__(self):
         self.options = None
@@ -34,21 +34,32 @@ class IsNorm(CommandBase, name="isnorm"):
 
     def run(self, df):
         options = self.options
-        selected_columns = df
+        selected_df = df
 
         if options.columns is not None:
             utils.validate_columns_error(df, options.columns)
-            selected_columns = df[options.columns]
+            selected_df = df[options.columns]
 
-        numeric_df = selected_columns.select_dtypes(include=np.number)
-        numeric_column_names = list(numeric_df.columns)
+        # select only the numeric columns
+        selected_df = selected_df.select_dtypes(include=np.number)
+        selected_columns = selected_df.columns
 
-        if numeric_column_names:
-            print("column,statistic,p-value")
-            for f in numeric_column_names:
-                k2, p_value = scipy.stats.normaltest(df[f]) 
-                print(f"{f},{k2},{p_value}")
-        return df
+        out_columns = []
+        out_stats = []
+        out_p_values = []
+
+        # process each column in turn, computing normaltest 
+        # we do each column separately so that we can handle NAs independently in each column
+        for column in selected_columns:
+            this_column = df[column]
+            this_notna = this_column.dropna()
+            k2, p_value = scipy.stats.normaltest(this_notna) 
+            out_columns.append(column)
+            out_stats.append(k2)
+            out_p_values.append(p_value)
+
+        result_df = pd.DataFrame({'column': out_columns, 'statistic': out_stats, 'p_value': out_p_values})
+        return result_df
 
 
 class Correlation(CommandBase, name="corr"):
