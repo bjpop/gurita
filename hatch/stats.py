50aa3de813e5f0e4e0a558348ef58bb1bc4109ed
Bernie Pope <bjpope@unimelb.edu.au>
2021-10-26 20:09:26 +1100

    Add outlier detection using IQR

--- hatch/stats.py
+++ hatch/stats.py
@@ -73,9 +73,9 @@ class Correlation(CommandBase, name="corr"):
         parser = argparse.ArgumentParser(usage=f'{self.name} -h | {self.name} <arguments>', add_help=True)
         parser.add_argument(
             '-c', '--columns', metavar='FEATURE', nargs="*", type=str, required=False,
-        help=f'Select only these columns (columns)')
+            help=f'Select only these columns (columns)')
         parser.add_argument('--method', required=False, default=const.DEFAULT_CORR_METHOD, choices=const.ALLOWED_CORR_METHODS,
-        help=f'Method for determining correlation. Allowed values: %(choices)s. Default: %(default)s.')
+            help=f'Method for determining correlation. Allowed values: %(choices)s. Default: %(default)s.')
         self.options = parser.parse_args(args)
 
     def run(self, df):
@@ -101,8 +101,8 @@ class Zscore(CommandBase, name="zscore"):
             '-c', '--columns', metavar='NAME', nargs="+", type=str, required=False,
             help=f'Select only these named columns. Only applies if --axis is "rows"')
         parser.add_argument(
-            '--zsuffix', required=False, default=const.DEFAULT_ZSCORE_SUFFIX,
-            help=f'Column label suffix for new z-score axes. Default: %(default)s.')
+            '--suffix', required=False, default=const.DEFAULT_ZSCORE_SUFFIX,
+            help=f'Label suffix for new z-score columns. Default: %(default)s.')
         self.options = parser.parse_args(args)
 
     def run(self, df):
@@ -122,8 +122,55 @@ class Zscore(CommandBase, name="zscore"):
             this_column = df[column]
             this_notna = this_column.dropna()
             this_z = scipy.stats.zscore(this_notna)
-            new_column = column + self.options.zsuffix
+            new_column = column + self.options.suffix
             df[new_column] = pd.Series(data=this_z, index=this_notna.index).reindex(df.index)
+        return df
+
+
+class Outlier(CommandBase, name="outlier"):
+    description = "Detect outliers in numerical columns using interquartile range."
+    category = "transformation"
+
+    def __init__(self):
+        self.options = None
+
+    def parse_args(self, args):
+        parser = argparse.ArgumentParser(usage=f'{self.name} -h | {self.name} <arguments>', add_help=True)
+        parser.add_argument(
+            '-c', '--columns', metavar='NAME', nargs="+", type=str, required=False,
+            help=f'Select only these named columns. Only applies if --axis is "rows"')
+        parser.add_argument(
+            '--suffix', required=False, default=const.DEFAULT_OUTLIER_COLUMN,
+            help=f'Label suffix for new outlier columns. Default: %(default)s.')
+        parser.add_argument(
+            '--iqrscale', metavar='S', required=False, type=float, default=const.DEFAULT_OUTLIER_IQR_SCALE,
+            help=f'Scale factor S for determining outliers. x < (Q1 - S*IQR) or x > (Q3 + S*IQR). Larger values return fewer outliers. Default: %(default)s.')
+        self.options = parser.parse_args(args)
+
+    def run(self, df):
+        options = self.options
+        selected_df = df
+        # optionally choose user specified columns
+        if options.columns is not None:
+            utils.validate_columns_error(df, options.columns)
+            selected_df = df[options.columns]
+
+        # select only the numeric columns
+        selected_df = selected_df.select_dtypes(include=np.number)
+        selected_columns = selected_df.columns
+ 
+        scale = options.iqrscale
+
+        # process each column in turn, computing z-score, adding new columns to the df 
+        # we do each column separately so that we can handle NAs independently in each column
+        for column in selected_columns:
+            this_column = selected_df[column]
+            q1 = this_column.quantile(0.25)
+            q3 = this_column.quantile(0.75)
+            iqr = q3 - q1 
+            outliers = (this_column < (q1 - scale * iqr)) | (this_column > (q3 + scale * iqr))
+            new_column_label = column + "_" + options.suffix
+            df[new_column_label] = outliers 
         return df
 
 
