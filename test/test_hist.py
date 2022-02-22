e978d6edb035f5220e9a83d20a051fb95b7d8ad8
Bernie Pope <bjpope@unimelb.edu.au>
2022-02-22 23:01:48 +1100

    Change output file format to png

--- test/test_hist.py
+++ test/test_hist.py
@@ -6,7 +6,7 @@ def test_hist_sepal_length_iris(capsys, tmpdir):
     #actual_out_file = Path(tmpdir, "test.hist.sepal_length.iris.svg")
     # XXX should really make this directory somewhere else
     Path("test/outputs").mkdir(parents=True, exist_ok=True)
-    actual_out_file = Path("test/outputs", "test.hist.sepal_length.iris.svg")
-    command = f"in data/iris.csv + hist -x sepal_length --format svg -o {str(actual_out_file)}"
-    expect_out_file = "test/expected/hist_sepal_length_iris.svg"
+    actual_out_file = Path("test/outputs", "test.hist.sepal_length.iris.png")
+    command = f"in data/iris.csv + hist -x sepal_length --format png -o {str(actual_out_file)}"
+    expect_out_file = "test/expected/hist_sepal_length_iris.png"
     test.utils.command_output(capsys, command, out_file=actual_out_file, expect_out_file=expect_out_file)
