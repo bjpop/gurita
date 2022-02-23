fd40f5539b1cbc4c0d568d0bb79dfaf18d570e7e
Bernie Pope <bjpope@unimelb.edu.au>
2022-02-23 14:46:37 +1100

    Try using SVG for testing

--- test/test_hist.py
+++ test/test_hist.py
@@ -2,11 +2,16 @@ import test.utils
 from pathlib import Path
 
 
-def test_hist_sepal_length_iris(capsys, tmpdir):
-    #actual_out_file = Path(tmpdir, "test.hist.sepal_length.iris.svg")
-    # XXX should really make this directory somewhere else
+def test_hist_sepal_length_iris_png(capsys, tmpdir):
     Path("test/outputs").mkdir(parents=True, exist_ok=True)
     actual_out_file = Path("test/outputs", "test.hist.sepal_length.iris.png")
     command = f"in data/iris.csv + hist -x sepal_length --format png -o {str(actual_out_file)}"
     expect_out_file = "test/expected/hist_sepal_length_iris.png"
+    test.utils.command_output(capsys, command, out_file=actual_out_file, expect_out_file=expect_out_file)
+
+def test_hist_sepal_length_iris_svg(capsys, tmpdir):
+    Path("test/outputs").mkdir(parents=True, exist_ok=True)
+    actual_out_file = Path("test/outputs", "test.hist.sepal_length.iris.svg")
+    command = f"in data/iris.csv + hist -x sepal_length --format svg -o {str(actual_out_file)}"
+    expect_out_file = "test/expected/hist_sepal_length_iris.svg"
     test.utils.command_output(capsys, command, out_file=actual_out_file, expect_out_file=expect_out_file)
